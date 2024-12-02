cb6316a7797bc61b78469089c952ee8788ce64b9
haikeyi <4voacdo@gmail.com>
2024-12-02 23:29:49 +0800

    removed background cursor effect on mobile

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ window.addEventListener('mousemove', (e) => {
 window.addEventListener('mouseout', (e) => {
     blob.style.display = none;
 })
+
+const cursor = document.getElementById("cursor");
+
+window.addEventListener('mousemove', (e) => {
+    console.log(e);
+    let x = e.pageX;
+    let y = e.pageY;
+    console.log(x, y)
+
+    cursor.animate({
+        left: `${x}px`,
+        top: `${y}px`
+    }, { duration: 3000, fill: "forwards" });
+});
+
+window.addEventListener('mouseout', (e) => {
+    cursor.style.display = none;
+})
